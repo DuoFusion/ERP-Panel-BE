8cfa9bf2aa9a043d31e5b83ba5910e27abd74753
shakilhkdigiverse-alt <shakilhkdigiverse@gmail.com>
2025-12-06 17:18:26 +0530

    company Push

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,8 +1,8 @@
 import { apiResponse, generateHash, HTTP_STATUS, isValidObjectId, USER_ROLES } from "../../common";
 import { userModel } from "../../database/model";
 import { countData, createOne, findAllAndPopulate, getDataWithSorting, getFirstMatch, reqInfo, responseMessage, updateData } from "../../helper";
-import { createUserSchema, deleteUserSchema, editUserSchema, getUserSchema } from "../../validation";
 import { userAccountDeletionModel } from "../../database/model/userAccountDeletion";
+import { createUserSchema, deleteUserSchema, editUserSchema, getUserSchema } from "../../validation";
 
 const ObjectId = require("mongoose").Types.ObjectId;
 
