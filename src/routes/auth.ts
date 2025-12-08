4138d03aed8a084bec290f4119ab8fe96f6b71a9
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2025-12-08 18:47:12 +0530

    routrs fodler remove

--- src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { authController } from "../controllers";
-
-const router = Router();
-
-router.post("/register", authController.register);
-router.post("/login", authController.login);
-
-export const AuthRoute = router;
